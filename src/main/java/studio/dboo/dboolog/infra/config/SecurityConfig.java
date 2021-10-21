e6c89c8c8b61607159d9171ab0ddacb809295f2a
dboo <dboo.studio@gmail.com>
2021-10-21 14:56:10 +0900

    jwt 인증방식을 베이스로 하도록 수정(2)

--- src/main/java/studio/dboo/dboolog/infra/config/SecurityConfig.java
+++ src/main/java/studio/dboo/dboolog/infra/config/SecurityConfig.java
@@ -2,16 +2,23 @@ package studio.dboo.dboolog.infra.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import studio.dboo.dboolog.infra.jwt.JwtAccessDeniedHandler;
+import studio.dboo.dboolog.infra.jwt.JwtAuthenticationEntryPoint;
+import studio.dboo.dboolog.infra.jwt.JwtFilter;
 import studio.dboo.dboolog.modules.accounts.AccountService;
 
 import javax.sql.DataSource;
@@ -26,6 +33,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final PasswordEncoder passwordEncoder;
     private final DataSource dataSource;
     private final AccountService accountService;
+
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtFilter jwtFilter;
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -42,6 +59,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        // Jwt 적용
+        http.csrf().disable()
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler);
+
+        // 세션 사용하지 않음
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         // 접근경로별 권한설정
         http.authorizeRequests()
@@ -67,14 +94,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 .antMatchers("api/account/authenticate").permitAll()
                 .anyRequest().authenticated();
-
-        //폼 로그인 설정
-        http.formLogin()
-            .loginPage("/view/login").permitAll();
-
-        //로그아웃 후 리다이렉션 설정
-        http.logout()
-                .logoutSuccessUrl("/");
     }
 }
 
